9955b99533d21ec2709336eef936f9e2414557fe
Jake Dennis <jake.dennis@intelice.com>
2018-09-27 12:30:35 -0400

    second

---
+++ LastLogin.psm1
@@ -0,0 +1,14 @@
+function Find-User {
+    New-Item -ItemType Directory -Force -Path C:\Temp
+    Get-ADUser -Filter 'Enabled -eq $true' -Properties Enabled, LastLogonDate | Select Name, Username, Enabled, LastLogonDate | Export-csv C:\Temp\LastLogonDate.csv
+    }
+    
+function Find-Comp {
+    New-Item -ItemType Directory -Force -Path C:\Temp
+    Get-ADComputer -Filter 'Enabled -eq $true' -Properties Enabled, LastLogonDate | Select Name, Username, Enabled, LastLogonDate | Export-csv C:\Temp\LastLogonDate.csv
+    }
+function GetInfo{
+    Get-WmiObject -ComputerName JDENNIS-SURFACE -Class Win32_BIOS
+    }
+    GetInfo -ComputerName localhost
+    
