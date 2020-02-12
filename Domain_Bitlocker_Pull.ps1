fa31a0fd457e1eb04b498e287164fd8ac0821149
Jake Dennis <jake-dennis@outlook.com>
2020-02-11 23:52:17 -0500

    Uploading domain configuration pull scripts

---
+++ Domain_Bitlocker_Pull.ps1
@@ -0,0 +1,2 @@
+﻿Get-AdObject -Filter "objectclass -eq 'msFVE-RecoveryInformation'" -Properties DistinguishedName, msFVE-RecoveryPassword, WhenCreated |
+Select-Object -Property @{n="ComputerName";e={$_.DistinguishedName.Split(',',2)[1]}}, msFVE-RecoveryPassword
