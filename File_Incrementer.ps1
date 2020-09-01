6801e2336ab154aa32d65c14f56b8fa6f7a4d7e4
Jake Dennis <jake-dennis@outlook.com>
2020-08-31 23:12:54 -0400

    Update File_Incrementer.ps1

--- File_Incrementer.ps1
+++ File_Incrementer.ps1
@@ -1,14 +1,4 @@
-$int = 0
-$intfor = [int]$int.ToString("00")
-foreach ($_ in 1..98){
-    $intfor++
-    Write-Host ("![alt text] (/posts/images/P0001/P0001_IMG$intfor.png)")
-}
-##Write-Host ("![alt text] (/posts/images/P0001/P0001_IMG$intfor.png)")
 
-
-
-#Write filename increaser
 $ObjectCount = Read-Host -Prompt "How many images do you want to tag with"
 $ObjectCount = [int]$ObjectCount
 $FrontText = Read-Host -Prompt "What text do you want to appear before the count"
@@ -20,23 +10,3 @@ foreach ($_ in 1..$ObjectCount){
     $intfor++
     Write-Host ($FrontText + $intfor.ToString("00") + $BackText)
 }
-
-
-
-
-Write-Host ($FrontText + $intfor + $BackText)
-
-
-New-Item -ItemType Directory -Force -Path $ScanLocation
-
-
-
-
-
-$int = 0
-$intfor = $int.ToString("00")
-
-foreach ($_ in $1..100){
-    [int]$intfor++
-}
-Write-Host ("![alt text] (/posts/images/P0001/P0001_IMG$intfor.png)")
