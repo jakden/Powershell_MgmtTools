fbc8498b7599b7855fe2e38d11b112171278f3f2
Jake Dennis <jake-dennis@outlook.com>
2022-10-28 21:45:41 -0400

    Update documentation

--- File_Incrementer.ps1
+++ File_Incrementer.ps1
@@ -1,6 +1,6 @@
-#An example of text that could come before when trying to create numbered Image files ![alt text](https://github.com/jakden/Hyper-V_Core_Setup/blob/main/images/P0002_IMG
-#An example of text that could come after when trying to create numbered Image files .png)
-#Which should spit out ![alt text](https://github.com/jakden/Hyper-V_Core_Setup/blob/main/images/P0002_IMG05.png) with incrementing IMG numbers
+#An example of text that could come before when trying to create numbered Image files for a markdown file ![alt text](https://github.com/jakden/Hyper-V_Core_Setup/blob/main/images/P0002_IMG
+#An example of text that could come after when trying to create numbered Image files for a markdown file .png)
+#Which should spit out ![alt text](https://github.com/jakden/Hyper-V_Core_Setup/blob/main/images/P0002_IMG05.png) or a local example ![alt text](/posts/images/P0001/P0001_IMG01.png) with incrementing IMG numbers
 $ObjectCount = Read-Host -Prompt "How many images do you want to tag with"
 $ObjectCount = [int]$ObjectCount
 $FrontText = Read-Host -Prompt "What text do you want to appear before the count"
